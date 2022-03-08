7e6c52801acb396b7286cff79c2c6c530a09cd43
Carlito Simon <933873@cognizant.com>
2022-03-08 21:02:54 +0800

    Add project files.

---
+++ Homework1/Tests/CalcTest.cs
@@ -0,0 +1,26 @@
+﻿using CognizantSoftvision.Maqs.BaseTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    /// <summary>
+    /// CalcTest test class
+    /// </summary>
+    [TestClass]
+    public class CalcTest : BaseTest
+    {
+        /// <summary>
+        /// Sample test
+        /// </summary>
+        [TestMethod]
+        public void AdditionTest()
+        {
+            int numOne = 1;
+            int numTwo = 2;
+
+            int sum = numOne + numTwo;
+            this.Log.LogMessage("Add Test");
+            Assert.AreEqual(3, sum, "Failed");
+        }
+    }
+}
